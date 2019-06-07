6c087ce8f4170be1502e06c3f0ce5c11c64e56cd
Lindsey Maddox <lindseyemaddox@gmail.com>
2019-06-07 10:37:09 -0500

    printing to dom, part-three complete

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,19 +1,25 @@
+// define an appropriately named variable that will have the value of an array
+
 const entries = [];
 
-let output = document.getElementById("entryLog");
+let output = document.getElementById("output");
+
+// use the push() method to add the journal entry objects you defined above to the array
 
 function addJournalEntry(entryObject) {
   entries.push(entryObject);
+  console.log("entries", entries);
   buildTheDom();
-}
+};
 
 let buildTheDom = () => {
+  // loop through entries
   output.innerHTML = "";
   for (var i = 0; i < entries.length; i++) {
     let journalEntry = entries[i];
     output.innerHTML += entryComponent(journalEntry);
-  }
-};
+  };
+}
 
 let entryComponent = (journalEntry) => {
   return `
@@ -23,10 +29,10 @@ let entryComponent = (journalEntry) => {
       <p>${journalEntry.entry}</p>
       <h3>Mood: ${journalEntry.mood}</h3>
     </div>
-  `;
-};
+  `
+}
 
-let submit = document.getElementById("submitEntry");
+let submit = document.getElementById("submit");
 
 function collectEntryItems() {
   const myJournalEntry = {
@@ -34,9 +40,10 @@ function collectEntryItems() {
     concepts: document.getElementById("concepts").value,
     entry: document.getElementById("entry").value,
     mood: document.getElementById("mood").value
-  };
+  }
   addJournalEntry(myJournalEntry);
-}
+};
+
 
 submit.addEventListener("click", function () {
   collectEntryItems();
