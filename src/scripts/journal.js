6f95d928637b288a70e6ef823befc0d0ab2bb490
Lindsey Maddox <lindseyemaddox@gmail.com>
2019-06-07 14:24:55 -0500

    wrong folder

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,25 +1,21 @@
-// define an appropriately named variable that will have the value of an array
 
 const entries = [];
 
 let output = document.getElementById("output");
 
-// use the push() method to add the journal entry objects you defined above to the array
-
 function addJournalEntry(entryObject) {
   entries.push(entryObject);
   console.log("entries", entries);
   buildTheDom();
-};
+}
 
 let buildTheDom = () => {
-  // loop through entries
   output.innerHTML = "";
   for (var i = 0; i < entries.length; i++) {
     let journalEntry = entries[i];
     output.innerHTML += entryComponent(journalEntry);
-  };
-}
+  }
+};
 
 let entryComponent = (journalEntry) => {
   return `
@@ -29,8 +25,8 @@ let entryComponent = (journalEntry) => {
       <p>${journalEntry.entry}</p>
       <h3>Mood: ${journalEntry.mood}</h3>
     </div>
-  `
-}
+  `;
+};
 
 let submit = document.getElementById("submit");
 
@@ -40,10 +36,9 @@ function collectEntryItems() {
     concepts: document.getElementById("concepts").value,
     entry: document.getElementById("entry").value,
     mood: document.getElementById("mood").value
-  }
+  };
   addJournalEntry(myJournalEntry);
-};
-
+}
 
 submit.addEventListener("click", function () {
   collectEntryItems();
